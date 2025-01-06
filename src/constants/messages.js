59ff70e32fcc3a150f33011654ec338d95a1a8ed
Bae seong-kyu <baesense@naver.com>
2025-01-06 23:47:53 +0900

    Refactor : 메세지 상수 분리

---
+++ src/constants/messages.js
@@ -0,0 +1,20 @@
+export const MESSAGES = {
+  LIGHTNING_SALE: (name) => `번개세일! ${name} 이(가) 20% 할인 중입니다!`,
+  SUGGESTION_SALE: (name) =>
+    `${name}은(는) 어떠세요? 지금 구매하시면 5% 추가 할인!`,
+  BONUS_POINT: (point) => `(포인트: ${point})`,
+  STOCK_INFO: (quantity) => `재고 부족 (${quantity}개 남음)`,
+  APPLY_DISCOUNT: (rate) => `(${rate}% 할인 적용)`,
+  OUT_STOCK: `재고가 부족합니다.`,
+  SHOPPING_BASKET: "장바구니",
+  SOLD_OUT: "품절",
+  ADD: "추가",
+  AMOUNT: "총액",
+  WON: "원",
+};
+
+export const SYMBOL = {
+  HYPHEN_MINUS: "-",
+  COLON: ":",
+  X: "x",
+};
